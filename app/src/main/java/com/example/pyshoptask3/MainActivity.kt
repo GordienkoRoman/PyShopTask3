54f1f891638cb978d5a698f968b2552b8f37727c
Roman Gordienko <trymen97@gmail.com>
2025-02-19 23:16:44 +0400

    Initial commit

---
+++ app/src/main/java/com/example/pyshoptask3/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.pyshoptask3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pyshoptask3.ui.theme.PyShopTask3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PyShopTask3Theme {
+        Greeting("Android")
+    }
+}
